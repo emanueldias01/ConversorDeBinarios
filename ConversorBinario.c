872a50940a30bab3fb12acda7f6c6d2f06553887
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-04-12 21:49:51 -0300

    return binary size

--- ConversorBinario.c
+++ ConversorBinario.c
@@ -1,23 +1,7 @@
 #include<stdio.h>
 
 void operacaoBinario(int numeroDecimal){
-    while(numeroDecimal/2 >= 1){
-        printf("%d", numeroDecimal%2);
-        if(numeroDecimal <= 2){
-            printf("%d", numeroDecimal/2);
-        }
-        numeroDecimal /=2;
-
-    }
-
-}
-
-int main(){
-
-    int numeroDecimal;
-    printf("Numero decimal a ser convertido: ");
-    scanf("%d", &numeroDecimal);
-
+    
 
     int numeroOpc = numeroDecimal;
     int tamanho = 0;
@@ -27,7 +11,33 @@ int main(){
         
     }
     tamanho++;
-    printf("%d\n\n", tamanho);
+    printf("tamanho: %d\n\n", tamanho);
+
+    int tamanhoVetorBinario = 0;
+
+    while(numeroDecimal/2 >= 1){
+        //printf(numeroDecimal%2);
+        tamanhoVetorBinario++;
+        if(numeroDecimal <= 2){
+            //printf("%d", numeroDecimal/2);
+            tamanhoVetorBinario++;
+        }
+        numeroDecimal /=2;
+
+    }
+    printf("tamanho do binario: %d", tamanhoVetorBinario);
+
+    int vetorBinario[tamanhoVetorBinario];
+
+
+}
+
+int main(){
+
+    int numeroDecimal;
+    printf("Numero decimal a ser convertido: ");
+    scanf("%d", &numeroDecimal);
+
 
     operacaoBinario(numeroDecimal);
 
