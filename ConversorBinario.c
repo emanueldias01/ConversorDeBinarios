ed01f91eed0a6c8678c25163e3b0fbfd82457b88
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-04-12 21:15:05 -0300

    catch size number

--- ConversorBinario.c
+++ ConversorBinario.c
@@ -2,5 +2,21 @@
 
 int main(){
 
+    int numeroDecimal;
+    printf("Numero decimal a ser convertido: ");
+    scanf("%d", &numeroDecimal);
+
+
+    int numeroOpc = numeroDecimal;
+    int tamanho = 0;
+    while (numeroOpc/10 > 0){
+        tamanho++;
+        numeroOpc /= 10;
+        
+    }
+    tamanho++;
+    printf("%d", tamanho);
+
+
     return 0;
 }
