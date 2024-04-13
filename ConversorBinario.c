0876b2e0de76c45f0aa077b93c85c08da1e289e4
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-04-12 22:20:26 -0300

    refactored code

--- ConversorBinario.c
+++ ConversorBinario.c
@@ -4,29 +4,19 @@ void operacaoBinario(int numeroDecimal){
     
     int numBinarioOpc = numeroDecimal;
     int numeroOpc = numeroDecimal;
-    int tamanho = 0;
-    while (numeroOpc/10 > 0){
-        tamanho++;
-        numeroOpc /= 10;
-        
-    }
-    tamanho++;
-    printf("tamanho: %d\n\n", tamanho);
 
     int tamanhoVetorBinario = 0;
 
     while(numeroDecimal/2 >= 1){
-        //printf(numeroDecimal%2);
         tamanhoVetorBinario++;
+
         if(numeroDecimal <= 2){
-            //printf("%d", numeroDecimal/2);
             tamanhoVetorBinario++;
+
         }
         numeroDecimal /=2;
 
     }
-    printf("tamanho do binario: %d\n", tamanhoVetorBinario);
-
 
 
     int vetorBinarioInvertido[tamanhoVetorBinario];
@@ -40,13 +30,6 @@ void operacaoBinario(int numeroDecimal){
         }
 
         numBinarioOpc /= 2;
-    }
-
-    printf("binario invertido: ");
-
-    for(int i = 0; i < contadorIndiceVetor; i++){
-        printf("%d", vetorBinarioInvertido[i]);
-
     }
 
     printf("\nEm binario: ");
@@ -66,8 +49,6 @@ void operacaoBinario(int numeroDecimal){
         printf("%d", binario[i]);
 
     }
-
-    printf("\ncontador indice: %d", contadorIndice);
 
 }
 
